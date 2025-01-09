a7c4bb3e4bcbd5c17be25ad890cc8bb7070e0188
fedealde <62813604+fedealde@users.noreply.github.com>
2025-01-09 17:44:40 -0300

    Actualizacion de salidas configurables con ancho de pulso por ADC y frecuencia aleatoria. RECORDAR, SALIDA ES INVERTIDA PERO PRINTF ESTA CORRECTO

--- main/main.c
+++ main/main.c
@@ -14,39 +14,80 @@
 
 // esp_err_t init_signals(void);
 
+#define UPDATE_INTERVAL 500 // Tiempo de encendido/apagado^
+#define QTY_INTERVAL_TO_SECOND (1000 / UPDATE_INTERVAL)
+#define MAX_COUNT_IN_SECONDS 20
+#define MAX_COUNT (MAX_COUNT_IN_SECONDS * QTY_INTERVAL_TO_SECOND)
+#define MS_OF_SENSOR_TIMER 20
+#define SHIFT_BITS_FOR_PERIODE 2 // Recorta el valor del ADC shifteando bits (12 bits usa)
+#define WIDTH_DEFAULT 300
+#define PERIODE_DEFAULT 1000
+
 
 static const char *TAG = "Main";
-TimerHandle_t xTimers;    // Handler de timer
-uint16_t interval = 1000; // Tiempo de encendido/apagado^
-const uint16_t max_count = 10;
+TimerHandle_t xTimers; // Handler de timer
 uint16_t count = 0;
-const uint8_t timerId = 1; // Identificador de timer
-volatile uint32_t duty_PIN_SEM_SENSOR_1 = 0;
-volatile uint32_t duty_PIN_SEM_SENSOR_2 = 0;
-int ADC_PIN_SEM_SENSOR_1 = 0;
-int ADC_PIN_SEM_SENSOR_2 = 0;
-int MIN_BW = 1;
-uint32_t rdm = 0;
-ledc_timer_config_t timerConfig_PIN_SEM_SENSOR_1 = {0};
-ledc_timer_config_t timerConfig_PIN_SEM_SENSOR_2 = {0};
-bool calibration_flag;
+const uint8_t timerId = 1, timerId_S = 2; // Identificador de timer
+volatile uint32_t width_sensor_1 = WIDTH_DEFAULT;
+volatile uint32_t width_sensor_2 = WIDTH_DEFAULT;
+volatile uint32_t period_sensor_1 = PERIODE_DEFAULT;
+volatile uint32_t period_sensor_2 = PERIODE_DEFAULT;
 
-// esp_err_t Define las funciones y permite retornar valores del estado del
-// funcionamiento de la ejecución de la funcion esp_err_t init_signals(void); //
-// defino la inicializacion
-esp_err_t set_timer(void);    // configura timer
-esp_err_t set_pwm(void);      // seteamos el pwm para cada led
-esp_err_t set_pwm_duty(void); // seteamos el duty para cada pwm
+int adc_sensor_1 = 0;
+int adc_sensor_2 = 0;
+int min_bw = 1;
+uint32_t rdm = 0;
+uint32_t toff = 0;
+
+
+esp_err_t gpio_init(void);
+esp_err_t set_timer(void);
+void vTimerCallback(TimerHandle_t pxTimer);
+void vTimerCallback_S(TimerHandle_t pxTimer);
+
+void app_main(void)
+{
+    vTaskDelay(100);
+    gpio_init();
+    set_timer();
+    ESP_ERROR_CHECK(adc_init());
+    rdm = esp_random();
+}
 
 void vTimerCallback(
     TimerHandle_t pxTimer) // tarea que ejecuta cada vez que termina el timer
 {
 
-adc_read(&ADC_PIN_SEM_SENSOR_1, 0);
-adc_read(&ADC_PIN_SEM_SENSOR_2, 1);
+    adc_read(&adc_sensor_1, 0);
+    adc_read(&adc_sensor_2, 1);
 
-    
-    if (count < max_count)
+    width_sensor_1 = ((uint32_t)adc_sensor_1) >> SHIFT_BITS_FOR_PERIODE;
+    width_sensor_2 = ((uint32_t)adc_sensor_2) >> SHIFT_BITS_FOR_PERIODE;
+
+    if (width_sensor_1 < min_bw)
+    {
+        width_sensor_1 = 0;
+        gpio_set_level(PIN_SEM_SENSOR_1_SIGNAL, 1);
+    }
+   if(period_sensor_1>width_sensor_1){
+    toff=period_sensor_1-width_sensor_1;
+    }else{toff=0;}
+
+    ESP_LOGE(TAG, "cant/min_1=%.1f period_sensor_1: %lu width_sensor_1 : %lu TOFF %lu",(float)(60*(1/((float)period_sensor_1/1000))), period_sensor_1, width_sensor_1, toff);
+
+    if (width_sensor_2 < min_bw)
+    {
+        width_sensor_2 = 0;
+        gpio_set_level(PIN_SEM_SENSOR_2_SIGNAL, 1);
+    }
+    if(period_sensor_2>width_sensor_2){
+    toff=period_sensor_2-width_sensor_2;
+    }else{toff=0;}
+
+    ESP_LOGE(TAG, "cant/min_2=%.1f period_sensor_2: %lu width_sensor_2 : %lu TOFF %lu",(float)(60*(1/((float)period_sensor_2/1000))), period_sensor_2, width_sensor_2, toff);
+
+
+    if (count < MAX_COUNT)
     {
         count++;
     }
@@ -54,57 +95,81 @@ adc_read(&ADC_PIN_SEM_SENSOR_2, 1);
     {
         count = 0;
         rdm = esp_random() & MASK_BYTE;
-        ESP_LOGI(TAG, "%lu", rdm);
+        if(rdm < width_sensor_1)
+        {
+            rdm = width_sensor_1;
+        }
+
+        period_sensor_1 = rdm;
+        ESP_LOGW(TAG, "rdm width_sensor_1 %lu", rdm);
+
+
+        rdm = esp_random() & MASK_BYTE;
+        if(rdm < width_sensor_2)
+        {
+            rdm = width_sensor_2;
+        }
+  
+        period_sensor_2 = rdm;
+        ESP_LOGW(TAG, "rdm width_sensor_2 %lu", rdm);
     }
 
-        duty_PIN_SEM_SENSOR_1 = ((uint32_t) ADC_PIN_SEM_SENSOR_1) >> 2;
-    if (rdm & (1 << BIT_MASK_SENSOR_1)) {
-        // ESP_LOGE(TAG, "0 - duty_PIN_SEM_SENSOR_1 : %d", duty_PIN_SEM_SENSOR_1);
-        timerConfig_PIN_SEM_SENSOR_1.freq_hz = 1;
-        // duty_PIN_SEM_SENSOR_1*=2;
-        ledc_timer_config(&timerConfig_PIN_SEM_SENSOR_1);
-    } else {
-        timerConfig_PIN_SEM_SENSOR_1.freq_hz = 2;
-        // duty_PIN_SEM_SENSOR_1/=2;
-        ledc_timer_config(&timerConfig_PIN_SEM_SENSOR_1);
-    }
-        if (duty_PIN_SEM_SENSOR_1 < MIN_BW) {
-            duty_PIN_SEM_SENSOR_1 = 0;
+}
+
+void vTimerCallback_S(
+    TimerHandle_t pxTimer) // tarea que ejecuta cada vez que termina el timer
+{
+
+    static uint32_t counter_width_1 = 0;
+    static uint32_t counter_width_2 = 0;
+    static uint32_t counter_period_1 = 0;
+    static uint32_t counter_period_2 = 0;
+
+    if (counter_period_1 < period_sensor_1)
+    {
+
+        counter_period_1+=MS_OF_SENSOR_TIMER;
+
+        if (counter_width_1 < width_sensor_1)
+        {
+            counter_width_1+=MS_OF_SENSOR_TIMER;
             gpio_set_level(PIN_SEM_SENSOR_1_SIGNAL, 0);
         }
-
-    ESP_LOGE(TAG, "f = %lu - duty_PIN_SEM_SENSOR_1 : %lu",timerConfig_PIN_SEM_SENSOR_1.freq_hz, duty_PIN_SEM_SENSOR_1);
-
-        duty_PIN_SEM_SENSOR_2 = ((uint32_t) ADC_PIN_SEM_SENSOR_2) >> 2;
-    if (rdm & (1 << BIT_MASK_SENSOR_2)) {
-        // ESP_LOGE(TAG, "0 - duty_PIN_SEM_SENSOR_2 : %d", duty_PIN_SEM_SENSOR_2);
-        timerConfig_PIN_SEM_SENSOR_2.freq_hz = 1;
-        // duty_PIN_SEM_SENSOR_2*=2;
-        ledc_timer_config(&timerConfig_PIN_SEM_SENSOR_2);
-    } else {
-        timerConfig_PIN_SEM_SENSOR_2.freq_hz = 2;
-        // duty_PIN_SEM_SENSOR_2/=2;
-        ledc_timer_config(&timerConfig_PIN_SEM_SENSOR_2);
+        else
+        {
+            gpio_set_level(PIN_SEM_SENSOR_1_SIGNAL, 1);
+        }
     }
-        if (duty_PIN_SEM_SENSOR_2 < MIN_BW) {
-            duty_PIN_SEM_SENSOR_2 = 0;
+    else
+    {
+        counter_period_1 = 0;
+        counter_width_1 = 0;
+        gpio_set_level(PIN_SEM_SENSOR_1_SIGNAL, 1);
+    }
+
+    if (counter_period_2 < period_sensor_2)
+    {
+
+        counter_period_2+=MS_OF_SENSOR_TIMER;
+
+        if (counter_width_2 < width_sensor_2)
+        {
+            counter_width_2+=MS_OF_SENSOR_TIMER;
             gpio_set_level(PIN_SEM_SENSOR_2_SIGNAL, 0);
         }
-
-    ESP_LOGE(TAG, "f = %lu - duty_PIN_SEM_SENSOR_2 : %lu", timerConfig_PIN_SEM_SENSOR_2.freq_hz, duty_PIN_SEM_SENSOR_2);
-   
-    set_pwm_duty();
+        else
+        {
+            gpio_set_level(PIN_SEM_SENSOR_2_SIGNAL, 1);
+        }
+    }
+    else
+    {
+        counter_period_2 = 0;
+        counter_width_2 = 0;
+        gpio_set_level(PIN_SEM_SENSOR_2_SIGNAL, 1);
+    }
 }
 
-void app_main(void)
-{
-    vTaskDelay(100);
-    // init_signals();
-    set_pwm();
-    set_timer();
-    ESP_ERROR_CHECK(adc_init());
-    rdm = esp_random();
-}
 
 esp_err_t set_timer(void)
 {
@@ -112,11 +177,31 @@ esp_err_t set_timer(void)
 
     // creamos el timer
     xTimers = xTimerCreate(
-        "Timer",                   // Just a text name, not used by the kernel.
-        (pdMS_TO_TICKS(interval)), // The timer period in ticks.
-        pdTRUE,                    // The timers will auto-reload themselves when they expire.
-        (void *)&timerId,          // Assign each timer a unique id equal to its array index.
-        vTimerCallback             // Each timer calls the same callback when it expires.
+        "Timer",                          // Just a text name, not used by the kernel.
+        (pdMS_TO_TICKS(UPDATE_INTERVAL)), // The timer period in ticks.
+        pdTRUE,                           // The timers will auto-reload themselves when they expire.
+        (void *)&timerId,                 // Assign each timer a unique id equal to its array index.
+        vTimerCallback                    // Each timer calls the same callback when it expires.
+    );
+
+    if (xTimers == NULL)
+    {
+        ESP_LOGE(TAG, "The timer was not created");
+    }
+    else
+    {
+        if (xTimerStart(xTimers, 0) != pdPASS)
+        {
+            ESP_LOGE(TAG, "The timer could not be set into the Active state");
+        }
+    }
+
+    xTimers = xTimerCreate(
+        "Timer_Sensor",                      // Just a text name, not used by the kernel.
+        (pdMS_TO_TICKS(MS_OF_SENSOR_TIMER)), // The timer period in ticks.
+        pdTRUE,                              // The timers will auto-reload themselves when they expire.
+        (void *)&timerId_S,                  // Assign each timer a unique id equal to its array index.
+        vTimerCallback_S                     // Each timer calls the same callback when it expires.
     );
 
     if (xTimers == NULL)
@@ -134,56 +219,16 @@ esp_err_t set_timer(void)
     return ESP_OK;
 }
 
-esp_err_t set_pwm(void)
+esp_err_t gpio_init(void)
 {
+    gpio_reset_pin(PIN_SEM_SENSOR_1_SIGNAL);
+    gpio_set_direction(PIN_SEM_SENSOR_1_SIGNAL, GPIO_MODE_OUTPUT);
+    gpio_set_level(PIN_SEM_SENSOR_1_SIGNAL, 1);
 
-    ledc_channel_config_t channelConfig_PIN_SEM_SENSOR_1 = {0};
-    channelConfig_PIN_SEM_SENSOR_1.gpio_num = PIN_SEM_SENSOR_1_SIGNAL;
-    channelConfig_PIN_SEM_SENSOR_1.speed_mode = LEDC_LOW_SPEED_MODE;
-    channelConfig_PIN_SEM_SENSOR_1.channel = LEDC_CHANNEL_1;
-    channelConfig_PIN_SEM_SENSOR_1.intr_type = LEDC_INTR_DISABLE;
-    channelConfig_PIN_SEM_SENSOR_1.timer_sel = LEDC_TIMER_1;
-    channelConfig_PIN_SEM_SENSOR_1.duty = 0;
-
-    ledc_channel_config_t channelConfig_PIN_SEM_SENSOR_2 = {0};
-    channelConfig_PIN_SEM_SENSOR_2.gpio_num = PIN_SEM_SENSOR_2_SIGNAL;
-    channelConfig_PIN_SEM_SENSOR_2.speed_mode = LEDC_LOW_SPEED_MODE;
-    channelConfig_PIN_SEM_SENSOR_2.channel = LEDC_CHANNEL_2;
-    channelConfig_PIN_SEM_SENSOR_2.intr_type = LEDC_INTR_DISABLE;
-    channelConfig_PIN_SEM_SENSOR_2.timer_sel = LEDC_TIMER_2;
-    channelConfig_PIN_SEM_SENSOR_2.duty = 0;
-
-    ledc_channel_config(&channelConfig_PIN_SEM_SENSOR_1);
-    ledc_channel_config(&channelConfig_PIN_SEM_SENSOR_2);
-
-    timerConfig_PIN_SEM_SENSOR_1.speed_mode = LEDC_LOW_SPEED_MODE;
-    timerConfig_PIN_SEM_SENSOR_1.duty_resolution = LEDC_TIMER_10_BIT;
-    timerConfig_PIN_SEM_SENSOR_1.timer_num = LEDC_TIMER_1;
-    timerConfig_PIN_SEM_SENSOR_1.clk_cfg = LEDC_AUTO_CLK;
-    timerConfig_PIN_SEM_SENSOR_1.freq_hz = 1;
- 
-    timerConfig_PIN_SEM_SENSOR_2.speed_mode = LEDC_LOW_SPEED_MODE;
-    timerConfig_PIN_SEM_SENSOR_2.duty_resolution = LEDC_TIMER_10_BIT;
-    timerConfig_PIN_SEM_SENSOR_2.timer_num = LEDC_TIMER_2;
-    timerConfig_PIN_SEM_SENSOR_2.clk_cfg = LEDC_AUTO_CLK;
-    timerConfig_PIN_SEM_SENSOR_2.freq_hz = 1;
-
-    ledc_timer_config(&timerConfig_PIN_SEM_SENSOR_1);
-    ledc_timer_config(&timerConfig_PIN_SEM_SENSOR_2);
+    gpio_reset_pin(PIN_SEM_SENSOR_2_SIGNAL);
+    gpio_set_direction(PIN_SEM_SENSOR_2_SIGNAL, GPIO_MODE_OUTPUT);
+    gpio_set_level(PIN_SEM_SENSOR_2_SIGNAL, 1);
 
     return ESP_OK;
 }
-
-esp_err_t set_pwm_duty(void)
-{
-    // Setea el duty
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, duty_PIN_SEM_SENSOR_1);
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_2, duty_PIN_SEM_SENSOR_2);
-    // Actualiza el duty
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1);
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_2);
-
-    return ESP_OK;
-}
-
 
--- main/main.h
+++ main/main.h
@@ -8,7 +8,7 @@
 // #define PIN_SEM_R_SIGNAL 33
 // #define PIN_SEM_G_SIGNAL 25
 
-#define MASK_BYTE 0x001F
+#define MASK_BYTE 0x07FF //Define la cantidad de bits que se van a utiizar para limitar el número random en rango, hay que mantenerlo igual al ADC para aprovechar el rango que se toma como ms
 #define BIT_MASK_R 0
 #define BIT_MASK_G 1
 
